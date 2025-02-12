191a3c0bbb573526bb73ff97c872506aa9de570a
Cristian Gomez <svpr0@hotmail.com>
2025-02-12 08:35:02 -0300

    Agrega modal, confetti y avatares

--- js/app.js
+++ js/app.js
@@ -3,9 +3,10 @@ const regex = /^[A-Za-zÁÉÍÓÚáéíóúÑñ ]+$/; // Expresión regular para
 let listaAmigos = [];
 
 // Elementos HTML
-const nuevoNombre = document.querySelector("#amigo");
+const nuevoNombre = document.querySelector("#nuevoNombreAmigo");
 const imprimirAmigos = document.querySelector("#listaAmigos");
 const imprimirSorteado = document.querySelector("#resultado");
+const botonAgregar = document.querySelector("#botonAgregarAmigo");
 const botonSortear = document.querySelector("#botonSortearAmigo");
 
 // Constantes para mensajes de error
@@ -16,6 +17,21 @@ const ERROR_NOMBRE_CORTO = "Por favor, inserte al menos 3 letras.";
 const ERROR_NOMBRE_DUPLICADO =
   "El nombre ya se encuentra en la lista de amigos.";
 
+// Eventos
+document.addEventListener("DOMContentLoaded", function () {
+  nuevoNombre.focus();
+  nuevoNombre.addEventListener("input", actualizarEstadoBoton);
+  document
+    .getElementById("nuevoNombreAmigo")
+    .addEventListener("keypress", (e) => {
+      if (e.key === "Enter" && nuevoNombre.value.trim() !== "") {
+        agregarAmigo();
+      } else if (e.key === "Enter" && nuevoNombre.value.trim() === "") {
+        sortearAmigo();
+      }
+    });
+});
+
 // Funciones
 function agregarAmigo() {
   console.log("Agregando amigo...");
@@ -23,31 +39,32 @@ function agregarAmigo() {
     console.log("Paso la verificacion nombre.");
     listaAmigos.push(capitalizarNombre(nuevoNombre.value));
     actualizarLista();
-    nuevoNombre.value = "";
     nuevoNombre.focus();
+    nuevoNombre.value = "";
     botonSortear.disabled = false;
     botonSortear.style.backgroundColor = "#fe652b";
+    botonAgregar.style.backgroundColor = "#c4c4c4";
   }
 }
 
 function verificarNombre(nombre) {
   if (nombre.trim() === "") {
-    alert(ERROR_NOMBRE_VACIO);
+    mostrarAlerta(ERROR_NOMBRE_VACIO);
     console.error(ERROR_NOMBRE_VACIO);
     return false;
   }
   if (!regex.test(nombre)) {
-    alert(ERROR_CARACTERES_INVALIDOS);
+    mostrarAlerta(ERROR_CARACTERES_INVALIDOS);
     console.error(ERROR_CARACTERES_INVALIDOS);
     return false;
   }
   if (nombre.length < 3) {
-    alert(ERROR_NOMBRE_CORTO);
+    mostrarAlerta(ERROR_NOMBRE_CORTO);
     console.error(ERROR_NOMBRE_CORTO);
     return false;
   }
   if (listaAmigos.includes(capitalizarNombre(nombre))) {
-    alert(ERROR_NOMBRE_DUPLICADO);
+    mostrarAlerta(ERROR_NOMBRE_DUPLICADO);
     console.error(ERROR_NOMBRE_DUPLICADO);
     return false;
   }
@@ -67,9 +84,9 @@ function capitalizarNombre(str) {
     .join(" ");
 }
 
-function quitarAmigo(amigo) {
-  let resultado = confirm(`¿Estás seguro de eliminar a ${amigo}?`);
-  if (resultado) {
+async function quitarAmigo(amigo) {
+  let resultado = await pedirConfirmacion(`¿Estás seguro de eliminar a ${amigo}?`);
+  if (resultado.isConfirmed) {
     listaAmigos = listaAmigos.filter((a) => a !== amigo);
     actualizarLista();
     console.log(`Amigo eliminado: ${amigo}`);
@@ -86,7 +103,11 @@ function actualizarLista() {
   imprimirAmigos.innerHTML = listaAmigos
     .map(
       (amigo) =>
-        `<li><img src="https://api.dicebear.com/9.x/adventurer-neutral/svg?radius=50&seed=${amigo}&size=32"> <span> ${amigo}</span> <i class="icono fa fa-trash fa-1x" aria-hidden="true" onclick="quitarAmigo('${amigo}')"></i></li>`
+        ` <li class="animate__animated animate__rubberBand">
+            <img src="https://api.dicebear.com/9.x/adventurer-neutral/svg?radius=50&seed=${amigo}&size=32" alt="Avatar de ${amigo}">
+            <span> ${amigo}</span>
+            <i class="icono fa fa-trash fa-1x" aria-hidden="true" onclick="quitarAmigo('${amigo}')"></i>
+          </li>`
     )
     .join("");
   console.log(`Lista actualizada: ${listaAmigos.join(", ")}`);
@@ -95,12 +116,65 @@ function actualizarLista() {
 function sortearAmigo() {
   if (listaAmigos.length > 0) {
     const amigoSorteado = Math.floor(Math.random() * listaAmigos.length);
-    imprimirSorteado.innerHTML = `Tu amigo secreto es: ${listaAmigos[amigoSorteado]}.`;
-    console.log(`Tu amigo secreto es: ${listaAmigos[amigoSorteado]}.`);
+    imprimirSorteado.innerHTML = `Tu amigo secreto es: ${listaAmigos[amigoSorteado]}`;
+    mostrarAlerta(
+      `Tu amigo secreto es:
+      ${listaAmigos[amigoSorteado]}`,
+      "success"
+    );
+    console.log(`Tu amigo secreto es: ${listaAmigos[amigoSorteado]}`);
+    mostrarConfetti();
   } else {
-    alert("No hay nombres en la lista de amigos.");
+    mostrarAlerta("No hay nombres en la lista de amigos.");
     console.error("No hay nombres en la lista de amigos.");
   }
+}
+
+function actualizarEstadoBoton() {
+  if (
+    nuevoNombre.value.length >= 3 &&
+    regex.test(nuevoNombre.value) &&
+    !listaAmigos.includes(capitalizarNombre(nuevoNombre.value))
+  ) {
+    botonAgregar.style.backgroundColor = "#4CdF50";
+  } else {
+    botonAgregar.style.backgroundColor = "#c4c4c4";
+  }
+}
+
+function mostrarAlerta(mensaje, tipo = "info") {
+  Swal.fire({
+    title: mensaje,
+    icon: tipo,
+    confirmButtonText: "OK",
+  });
+}
+
+function pedirConfirmacion(mensaje) {
+  return Swal.fire({
+    title: mensaje,
+    icon: "question",
+    showCancelButton: true,
+    confirmButtonText: "Sí",
+    cancelButtonText: "No",
+  });
+}
+
+function mostrarConfetti() {
+  confetti({
+    particleCount: 500,
+    spread: 360,
+    origin: { y: 0.6 },
+    colors: [
+      "#fee0be",
+      "#5ff9b1",
+      "#ff8f9b",
+      "#eba1ff",
+      "#a3d6ff",
+      "#e0f2f7",
+      "#ff8f4f",
+    ],
+  });
 }
 
 function reiniciarJuego() {
